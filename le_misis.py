6680ad45823a188167d64fb06a336bcd398a5b32
Алексей Супрун <a.suprun@cian.ru>
2025-04-05 16:24:43 +0300

    le_mesis 2 point

--- le_misis.py
+++ le_misis.py
@@ -3,13 +3,22 @@ import math
 from clover import srv
 from std_srvs.srv import Trigger
 
+
+
 rospy.init_node('flight')
-
 get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
-navigate = rospy.ServiceProxy('navigate', srv.Navigate)
-land = rospy.ServiceProxy('land', Trigger)
+navigate = rospy.ServiceProxy('navigate', srv.Navigate) # Отправиться в точку
+navigate_global = rospy.ServiceProxy('navigate_global', srv.NavigateGlobal)
+set_altitude = rospy.ServiceProxy('set_altitude', srv.SetAltitude)
+set_yaw = rospy.ServiceProxy('set_yaw', srv.SetYaw)
+set_yaw_rate = rospy.ServiceProxy('set_yaw_rate', srv.SetYawRate)
+set_position = rospy.ServiceProxy('set_position', srv.SetPosition)
+set_velocity = rospy.ServiceProxy('set_velocity', srv.SetVelocity)
+set_attitude = rospy.ServiceProxy('set_attitude', srv.SetAttitude)
+set_rates = rospy.ServiceProxy('set_rates', srv.SetRates)
+land = rospy.ServiceProxy('land', Trigger) #для посадки
 
-def navigate_wait(x=0, y=0, z=1, yaw=float('nan'), speed=0.2, frame_id='aruco_map', auto_arm=False, tolerance=0.2):
+def navigate_wait(x=0, y=0, z=1, yaw=float('nan'), speed=0.5, frame_id='aruco_map', auto_arm=False, tolerance=0.2):
     navigate(x=x, y=y, z=z, yaw=yaw, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
 
     while not rospy.is_shutdown():
@@ -19,9 +28,13 @@ def navigate_wait(x=0, y=0, z=1, yaw=float('nan'), speed=0.2, frame_id='aruco_ma
         rospy.sleep(0.2)
 
 navigate_wait(frame_id='body', auto_arm=True)
-rospy.sleep(5)
+rospy.sleep(3)
 navigate_wait(frame_id='aruco_103')
-rospy.sleep(5)
-navigate_wait(z=0.3, frame_id='body')
-rospy.sleep(5)
+rospy.sleep(3)
+navigate_wait(frame_id='aruco_104')
+rospy.sleep(3)
+navigate_wait(frame_id='body')
+rospy.sleep(3)
+navigate_wait(z=0.5, frame_id='body')
+rospy.sleep(3)
 land()
